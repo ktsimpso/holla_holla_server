c44a9c10af232e083edbfd3c069630b702cc1de6
Kevin Simpson <ktsimpso@gmail.com>
2014-05-18 17:07:43 -0400

    Added stores. Some refactoring. Null json convert to empty list.

--- models/models.go
+++ models/models.go
@@ -5,34 +5,62 @@ import (
 	"github.com/coocood/qbs"
 )
 
+type T interface{}
+type GetFunction func() (T, error)
+
 func RegisterDb() {
 	qbs.Register("postgres", "user=holla dbname=hollaholla password=gimmiechocolate sslmode=disable", "hollaholla", qbs.NewPostgres())	
 }
 
 type User struct {
-	Id int64
-	Name string `qbs:"size:64,index"`
+	Id int64 `json:"id"`
+	Name string `qbs:"size:64,index"json:"name"`
 }
 
-func CreateUserTable() error {
+type Store struct {
+	Id int64 `json:"id"`
+	Name string `qbs:"size:64,index"json:"name"`
+}
+
+//TODO: handle errors
+func CreateTables() error {
+	createTable(new(User))
+	createTable(new(Store))
+	return nil
+}
+
+func createTable(t T) error {
 	migration, err := qbs.GetMigration()
 	if err != nil {
 		return err
 	}
 
 	defer migration.Close()
-	return migration.CreateTableIfNotExists(new(User))
+	return migration.CreateTableIfNotExists(t)
 }
 
-func GetUsers() ([]*User, error) {
+func GetUsers() (T, error) {
 	var users []*User;
 
 	q, err := qbs.GetQbs()
 	if err != nil {
-		return users, err
+		return nil, err
 	}
 	defer q.Close()
 
 	err = q.FindAll(&users)
 	return users, err
+}
+
+func GetStores() (T, error) {
+	var stores []*Store;
+
+	q, err := qbs.GetQbs()
+	if err != nil {
+		return nil, err
+	}
+	defer q.Close()
+
+	err = q.FindAll(&stores)
+	return stores, err
 }
--- server.go
+++ server.go
@@ -4,31 +4,57 @@ import (
 	"github.com/go-martini/martini"
 	"github.com/ktsimpso/holla_holla_server/models"
 	"encoding/json"
+	"net/http"
 )
 
 func main() {
 	models.RegisterDb()
-	models.CreateUserTable()
+	models.CreateTables()
 
 	m := martini.Classic()
-	m.Get("/", func () string {
-		return "Hello World!"
+
+	m.Use(func(c martini.Context, w http.ResponseWriter) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	})
 
 	m.Get("/user", func () (int, string) {
-		users, err := models.GetUsers()
+		users, err := packIntoJson(models.GetUsers)
 		if err != nil {
 			return 500, "An error occured!"
 		}
 
-		data, err := json.Marshal(users)
+		return 200, users
+	})
 
+	m.Get("/store", func () (int, string) {
+		stores, err := packIntoJson(models.GetStores)
 		if err != nil {
-			return 500, "An error occured!!"
+			return 500, "An error occured!"
 		}
 
-		return 200, string(data)
+		return 200, stores
 	})
 
 	m.Run()
+}
+
+func packIntoJson(get models.GetFunction) (string, error) {
+	items, err := get()
+
+	if err != nil {
+		return "", err
+	}
+
+	data, err := json.Marshal(items)
+
+	if err != nil {
+		return "", err
+	}
+
+	stringData := string(data)
+	if stringData == "null" {
+		stringData = "[]"
+	}
+
+	return stringData, nil
 }
