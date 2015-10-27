23ba27103e799c3e99b61fa3644d17052d6bf027
Kevin Simpson <ktsimpso@gmail.com>
2015-10-27 00:02:21 -0700

    Switched from martini to echo.

--- README.md
+++ README.md
@@ -7,15 +7,7 @@ Install Dependances:
 
 Enter the following in your project root:
 
-`go get github.com/coocood/qbs`  
-`go get github.com/go-martini/martini`  
-`go get github.com/lib/pq`  
-`go get github.com/martini-contrib/gzip`  
-`go get github.com/codegangsta/martini-contrib/cors`  
-
-Note the following require Mercurial  
-`go get code.google.com/p/go.crypto/bcrypt`  
-`go get code.google.com/p/go.crypto/pbkdf2`  
+`go get -u ./...`  
 
 Download and install Postgres:
 
--- server.go
+++ server.go
@@ -2,57 +2,53 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/codegangsta/martini-contrib/cors"
-	"github.com/go-martini/martini"
 	"github.com/ktsimpso/holla_holla_server/models"
-	"github.com/martini-contrib/gzip"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/rs/cors"
 	"net/http"
 )
 
 func main() {
-	m := martini.New()
-	m.Use(martini.Logger())
-	m.Use(gzip.All())
-	m.Use(martini.Recovery())
-	m.Use(cors.Allow(&cors.Options{
-		AllowAllOrigins: true,
-	}))
-
-	m.Use(func(c martini.Context, w http.ResponseWriter) {
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	e := echo.New()
+	e.StripTrailingSlash()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.Recover())
+	e.Use(cors.Default().Handler)
+	e.Use(func(c *echo.Context) error {
+		c.Response().Header().Set("Content-Type", "application/json; charset=utf-8")
+		return nil
 	})
 
-	r := martini.NewRouter()
-
-	r.Get("/user", func() (int, string) {
+	e.Get("/user", func(c *echo.Context) error {
 		users, err := packIntoJson(models.GetUsers)
 		if err != nil {
-			return 500, "An error occured!"
+			return err
 		}
 
-		return 200, users
+		return c.String(http.StatusOK, users)
 	})
 
-	r.Get("/store", func() (int, string) {
+	e.Get("/store", func(c *echo.Context) error {
 		stores, err := packIntoJson(models.GetStores)
 		if err != nil {
-			return 500, "An error occured!"
+			return err
 		}
 
-		return 200, stores
+		return c.String(http.StatusOK, stores)
 	})
 
-	r.Get("/deal", func() (int, string) {
+	e.Get("/deal", func(c *echo.Context) error {
 		deals, err := packIntoJson(models.GetDeals)
 		if err != nil {
-			return 500, "An error occured"
+			return err
 		}
 
-		return 200, deals
+		return c.String(http.StatusOK, deals)
 	})
 
-	m.Action(r.Handle)
-	m.Run()
+	e.Run(":3000")
 }
 
 func packIntoJson(get models.GetFunction) (string, error) {
